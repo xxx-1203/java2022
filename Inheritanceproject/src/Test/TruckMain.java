f44a73d6a756c9da25d51e7b5853464c82c8cebf
sunhey kim <jinxianhuijxh@naver.com>
2022-04-08 09:58:42 +0900

    수정함

--- Inheritanceproject/src/Test/TruckMain.java
+++ Inheritanceproject/src/Test/TruckMain.java
@@ -2,9 +2,11 @@ package Test;
 
 class Truckmain {
 	public static void main(String[] args){
-		Truck mytruck = new Truck();
+		Truck mytruck = new Truck(); //ny truk클래스를 가리키는 개채변수
+		//car클래스에 있는 모든 맵버에 접근가능
 		mytruck.carname = "프론티어";
 		mytruck.ton = 3;
+		mytruck.velocity = 100;
 		System.out.println("나의 트럭은 " + mytruck.color + "이다.");
 		System.out.println(mytruck.carname+"는 "+mytruck.ton +"톤을 실을 수 있다");
 	}
